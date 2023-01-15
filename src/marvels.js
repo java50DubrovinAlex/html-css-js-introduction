2ae5a31d0a72466478b9b0095e8f3c1fce5684be
Alexander <caxagsh@gmail.com>
2023-01-15 21:58:08 +0200

    last ver 1

--- src/marvels.js
+++ src/marvels.js
@@ -7,43 +7,91 @@ const wordElement = document.querySelector(".word");
 const resultMessage = document.getElementById("result-message");
 const playAgain = document.getElementById("play-again");
 let wordLettersElement;
-//global variables
-const questionsWords = [["question1", "answer1"], ["question2","answer2"]];
+const questionsWords = [["The best soccer player in the world?", "Messi"], ["Where was Charlie Chaplin born?","London"], ["What is the capital of Scotland?", "Edinburg"], ["What is the capital of Wales?","Cardiff"]];
 // let currentIndex = 0;
 let initialLettersNumber;
 let remainedLettersNumber;
+let numOfCurGame = 0;
 //functions
-let numOfCurGame = 0; 
-numOfCurGame = finishGame() ? numOfCurGame++ : numOfCurGame;
+ 
+
 function startGame(){
+    letterInputElement.readOnly = false;
     wordInputElement.value = '';
     wordInputElement.readOnly = true;
     questionElement.innerHTML = questionsWords[numOfCurGame][0];
     playAgain.style.display='none'
     resultMessage.innerHTML='';
     wordElement.innerHTML = getWordDivs(questionsWords[numOfCurGame][1]);
-
+    remainedLettersNumber = Math.round (questionsWords[numOfCurGame][1].length / 3);
+    wordLettersElement = document.querySelectorAll(".letter");
+    remainedLettersElement.innerHTML = `letters remained ${remainedLettersNumber}`;
 }
 
 function getWordDivs(questionsWords) {
     //TODO
-    let res;
-    for(let i = 0 ; i < questionsWords.lenght; i++){
+    let res="";
+    for(let i = 0 ; i < questionsWords.length; i++){
         res = res + `<div class="letter"></div>`
     }
-    wordLettersElement = document.querySelectorAll(".letter");
+    
+    return res;
 }
 function checkWord() {
   //TODO
+  
+  let isWin = questionsWords[numOfCurGame][1].toLowerCase() === wordInputElement.value ? true : false;
+  finishGame(isWin);
+    
+  
+  wordInputElement.readOnly = true;
 }
 function processLetter() {
     //TODO
+    const answerWord = questionsWords[numOfCurGame][1];
+    let inputLetter = document.getElementById("letter-input").value;
+    if(!remainedLettersNumber){
+        letterInputElement.readOnly = true;
+    } else {
+        
+        for(let i = 0; i < questionsWords[numOfCurGame][1].length; i++){
+            if(inputLetter == answerWord[i].toLowerCase()){
+                wordLettersElement[i].innerHTML = answerWord[i];
+                wordLettersElement[i].style.background = "white";
+                remainedLettersNumber--;
+            }
+        }
+        
+    }
+    remainedLettersElement.innerHTML = `letters remained ${(remainedLettersNumber < 0) ? remainedLettersNumber = 0 : remainedLettersNumber}`;
+    letterInputElement.value = "";
+
 }
 function takeChance() {
     //TODO
+    wordInputElement.readOnly=false;
+    letterInputElement.readOnly = true;
+
+
 }
-function finishGame() {
+function finishGame(winOrLose) {
     //TODO
+   
+    const answerWord = questionsWords[numOfCurGame][1];
+    for(let i = 0; i < answerWord.length; i++){
+        wordLettersElement[i].innerHTML = answerWord[i];
+        wordLettersElement[i].style.background = "white";
+    }
+    numOfCurGame++;
+    if(numOfCurGame > questionsWords.length - 1){
+        numOfCurGame = 0;
+    }
+    playAgain.style.display = "flex"
+    
+    winOrLose ? resultMessage.innerHTML= "*** YOU WIN !!! ***" : resultMessage.innerHTML= "*** YOU LOSE !!! ***";
+    
+    
 }
+
 //actions
 startGame();
